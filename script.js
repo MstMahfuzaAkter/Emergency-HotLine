4e481fd48aa4282df7d3333aa75fbaf1aa80a980
MstMahfuzaAkter <mstmahfuzaker581@gamil.com>
2025-08-28 20:58:25 +0600

    added the call history card and clear button js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function getElementClass(cls) {
     return document.getElementsByClassName(cls);
 }
 let hearts = getElementClass("heart");
-
-// প্রতিটা heart এ event listener বসানো
 for (let heart of hearts) {
     heart.addEventListener("click", function (e) {
         e.preventDefault();
@@ -16,3 +14,39 @@ for (let heart of hearts) {
         getElement("count-heart").innerText = count_heart;
     });
 }
+
+getElement("product-box").addEventListener("click",
+    function (e) {
+        if (e.target.className.includes("call-btn")) {
+            const callButton = e.target;
+            const cardTitle = callButton.parentNode.parentNode.parentNode.children[1].innerText
+            const callNumber = callButton.parentNode.parentNode.parentNode.children[3].innerText
+            const data = {
+                date: new Date().toLocaleTimeString()
+            }
+            const cartContainer = getElement("cart-container");
+            const newcart = document.createElement("div");
+            newcart.innerHTML = `
+            <div class="flex items-center justify-between shadow-sm bg-[var(--Surface,rgba(250,250,250,1))]  rounded-sm  mt-5">
+                            <div>
+                                <p class="text-[var(--Dark, rgba(17, 17, 17, 1))] font-semibold p-3 pb-1">${cardTitle}
+                                </p>
+                                <p class="text-gray-400 font-bold p-3 pt-0">${callNumber}</p>
+                            </div>
+                            <div class="pr-3 text-[var(--Dark, rgba(17, 17, 17, 1))] font-semibold ">
+                                <p>${data.date}</p>
+                            </div>
+                        </div>
+                        
+        `;
+            cartContainer.append(newcart);
+
+        }
+    }
+);
+
+getElement("clear-btn").
+    addEventListener("click", function (e) {
+        e.preventDefault();
+        getElement("cart-container").innerText = " ";
+    })
