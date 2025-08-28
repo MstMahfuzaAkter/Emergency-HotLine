1189c58529bfd761c79ddf868e5449fbc6b9a2ce
MstMahfuzaAkter <mstmahfuzaker581@gamil.com>
2025-08-28 22:15:53 +0600

    added some js work

--- script.js
+++ script.js
@@ -15,35 +15,44 @@ for (let heart of hearts) {
     });
 }
 
-getElement("product-box").addEventListener("click",
-    function (e) {
-        if (e.target.className.includes("call-btn")) {
-            const callButton = e.target;
-            const cardTitle = callButton.parentNode.parentNode.parentNode.children[1].innerText
-            const callNumber = callButton.parentNode.parentNode.parentNode.children[3].innerText
-            const data = {
-                date: new Date().toLocaleTimeString()
-            }
-            const cartContainer = getElement("cart-container");
-            const newcart = document.createElement("div");
-            newcart.innerHTML = `
-            <div class="flex items-center justify-between shadow-sm bg-[var(--Surface,rgba(250,250,250,1))]  rounded-sm  mt-5">
-                            <div>
-                                <p class="text-[var(--Dark, rgba(17, 17, 17, 1))] font-semibold p-3 pb-1">${cardTitle}
-                                </p>
-                                <p class="text-gray-400 font-bold p-3 pt-0">${callNumber}</p>
-                            </div>
-                            <div class="pr-3 text-[var(--Dark, rgba(17, 17, 17, 1))] font-semibold ">
-                                <p>${data.date}</p>
-                            </div>
-                        </div>
-                        
-        `;
-            cartContainer.append(newcart);
+getElement("product-box").addEventListener("click", function (e) {
+    if (e.target.className.includes("call-btn")) {
+        const callButton = e.target;
+        const cardTitle = callButton.parentNode.parentNode.parentNode.children[1].innerText;
+        const cardSubtitle = callButton.parentNode.parentNode.parentNode.children[2].innerText;
+        const callNumber = callButton.parentNode.parentNode.parentNode.children[3].innerText;
 
+        let coinElement = getElement("coin-count");
+        let currentCoin = Number(coinElement.innerText);
+        if (currentCoin < 20) {
+            alert(" ❌ আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০টা কয়েন লাগবে।");
+            return;
         }
+        currentCoin -= 20;
+        coinElement.innerText = currentCoin;
+
+        alert(`📞 Calling ${cardSubtitle}  ${callNumber}....`);
+
+        const data = {
+            date: new Date().toLocaleTimeString()
+        };
+
+        const cartContainer = getElement("cart-container");
+        const newCart = document.createElement("div");
+        newCart.innerHTML = `
+            <div class="flex items-center justify-between shadow-sm bg-[var(--Surface,rgba(250,250,250,1))]  rounded-sm  mt-5">
+                <div>
+                    <p class="text-[var(--Dark, rgba(17, 17, 17, 1))] font-semibold p-3 pb-1">${cardTitle}</p>
+                    <p class="text-gray-400 font-bold p-3 pt-0">${callNumber}</p>
+                </div>
+                <div class="pr-3 text-[var(--Dark, rgba(17, 17, 17, 1))] font-semibold ">
+                    <p>${data.date}</p>
+                </div>
+            </div>
+        `;
+        cartContainer.append(newCart);
     }
-);
+});
 
 getElement("clear-btn").
     addEventListener("click", function (e) {
